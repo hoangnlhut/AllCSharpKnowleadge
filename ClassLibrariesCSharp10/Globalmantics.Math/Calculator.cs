b8dfe7de956b3c880c904a8639aec4b65a803059
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2024-03-21 09:23:07 +0700

    introduction of library project

---
+++ ClassLibrariesCSharp10/Globalmantics.Math/Calculator.cs
@@ -0,0 +1,8 @@
+﻿namespace Globalmantics.Math
+{
+    public static class Calculator
+    {
+        public static int Add(int a, int b) => a + b;
+
+    }
+}
--- ClassLibrariesCSharp10/Program.cs
+++ ClassLibrariesCSharp10/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace ClassLibrariesCSharp10
+﻿using Globalmantics.Math;
+
+namespace ClassLibrariesCSharp10
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the Globomantics calculator!");
+            Console.WriteLine();
+
+            // Input validation omitted to keep demo code simple
+            Console.WriteLine("Please enter your first integer number");
+            int number1 = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("Please enter an operation");
+            string? operation = Console.ReadLine();
+
+            Console.WriteLine("Please enter your second integer number");
+            int number2 = int.Parse(Console.ReadLine()!);
+
+            if (operation == "+")
+            {
+                var result = Calculator.Add(number1, number2);
+                Console.WriteLine($"The result is: {result}");
+            }
+            else
+            {
+                Console.WriteLine($"The operation '{operation}' is not currently supported.");
+            }
+
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
         }
     }
 }
+
+
+
