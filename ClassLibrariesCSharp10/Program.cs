bcb92ea149fb26bda05d1ca0aedfc6e201e2d8bd
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2024-03-22 11:35:10 +0700

    add some code for configuring building class libraries and support multiple net version in class library

--- ClassLibrariesCSharp10/Program.cs
+++ ClassLibrariesCSharp10/Program.cs
@@ -6,31 +6,39 @@ namespace ClassLibrariesCSharp10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Globomantics calculator!");
-            Console.WriteLine();
-
-            // Input validation omitted to keep demo code simple
-            Console.WriteLine("Please enter your first integer number");
-            int number1 = int.Parse(Console.ReadLine()!);
-
-            Console.WriteLine("Please enter an operation");
-            string? operation = Console.ReadLine();
-
-            Console.WriteLine("Please enter your second integer number");
-            int number2 = int.Parse(Console.ReadLine()!);
-
-            if (operation == "+")
+            #if DEBUG
             {
-                var result = Calculator.Add(number1, number2);
-                Console.WriteLine($"The result is: {result}");
-            }
-            else
-            {
-                Console.WriteLine($"The operation '{operation}' is not currently supported.");
-            }
+                Console.WriteLine("Welcome to the Globomantics calculator!");
+                Console.WriteLine();
 
-            Console.WriteLine("Press enter to exit");
-            Console.ReadLine();
+                // Input validation omitted to keep demo code simple
+                Console.WriteLine("Please enter your first integer number");
+                int number1 = int.Parse(Console.ReadLine()!);
+
+                Console.WriteLine("Please enter an operation");
+                string? operation = Console.ReadLine();
+
+                Console.WriteLine("Please enter your second integer number");
+                int number2 = int.Parse(Console.ReadLine()!);
+
+                if (operation == "+")
+                {
+                    var result = Calculator.Add(number1, number2);
+                    Console.WriteLine($"The result is: {result}");
+                    Console.WriteLine($"The hex result is: {Calculator.AsHex(result)}");
+
+                }
+                else
+                {
+                    Console.WriteLine($"The operation '{operation}' is not currently supported.");
+                }
+
+                Console.WriteLine("Press enter to exit");
+                Console.ReadLine();
+            }
+            #else
+             Console.WriteLine("This is release mode");
+            #endif
         }
     }
 }
